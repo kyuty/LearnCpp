45f621e074aa367b2fba95a97a766ee90f50bed6
wangdong <wangdong@smartisan.com>
2017-11-22 23:46:53 +0800

    Add my study folder.

---
+++ code/study/wd_cassert/wd_cassert.cpp
@@ -0,0 +1,28 @@
+/* assert example */
+#include <stdio.h>      /* printf */
+#include <assert.h>     /* assert */
+
+void print_number(int* myInt) {
+  assert (myInt!=NULL);
+  printf ("%d\n",*myInt);
+}
+
+int main ()
+{
+  int a=10;
+  int * b = NULL;
+  int * c = NULL;
+
+  b=&a;
+
+  print_number (b);
+  print_number (c);
+
+  return 0;
+}
+
+/*
+10
+Assertion failed: (myInt!=NULL), function print_number, file wd_cassert.cpp, line 6.
+Abort trap: 6
+ */
